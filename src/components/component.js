e78957b12592d8d24595f0d9bdc716ae2ef76512
Piotr <piotr@MacBook-Pro-Piotr.local>
2021-03-16 08:28:20 +0100

    scss modules test

---
+++ src/components/component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as styles from "./component.module.scss";
+
+const Component = () => {
+  return <p className={styles.nowa}>123</p>;
+};
+
+export default Component;
